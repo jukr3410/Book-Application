a5012bc1f14a165414a508eff16b39b392870df2
pimpcnn <56501828+jukr3410@users.noreply.github.com>
2021-12-16 11:22:24 +0700

    [update]

--- src/main/java/com/example/bookapp/data/api/ApiService.kt
+++ src/main/java/com/example/bookapp/data/api/ApiService.kt
@@ -1,11 +1,14 @@
 package com.example.bookapp.data.api
 
+import com.example.bookapp.Constants
 import com.example.bookapp.model.BookResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("books.json?api-key=GDrQ2aBDKGj6DELALg9H4JeXLysN1peW")
-    suspend fun getBooks(): Response<BookResponse>
+    @GET("books.json?api-key="+Constants.API_KEY)
+    suspend fun getBooks(
+    ): Response<BookResponse>
 }
